8671d93be4102125ad73b48f8111ba3a1c1e812d
maggiojoe2 <thesealion95@gmail.com>
2020-02-13 14:56:11 -0600

    Set Up reactfrontend Docker (not yet tested in docker)

--- reactfrontend/src/index.js
+++ reactfrontend/src/index.js
@@ -4,10 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
-
-//Footer (appears on all pages)
-// ReactDOM.render(<Footer />, document.getElementById('footer'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
