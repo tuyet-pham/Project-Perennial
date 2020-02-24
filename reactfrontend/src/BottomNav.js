b465a481a2cb48a41644b9f7d4875804246cb430
Peyton Pritchard <pritchardpeyton119@gmail.com>
2020-02-24 13:37:44 -0600

    Initialized some test files - still trying to iron out the basic stuff.

---
+++ reactfrontend/src/BottomNav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function BottomNav() {
+    return (
+        <div className="Bottom-Nav">
+            <p>FOOTER</p>
+        </div>
+    )
+}
---
+++ reactfrontend/src/BottomNav.test.js
@@ -0,0 +1,46 @@
+import ReactDOM, { unmountComponentAtNode } from "react-dom";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import BottomNav from './BottomNav';
+
+// Setup and teardown
+let container = null;
+beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+})
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+})
+
+it('renders', () => {
+    act(() => {
+        render('BottomNav',container);
+    });
+    expect(container.textContent).toBe('FOOTER')
+})
+
+/*
+// Check that it redirects to the home page.
+test("redirects to home page", () => {
+    // Render the app
+    render(
+      <MemoryRouter initialEntries={['/initial_route']}>
+        <BottomNav />
+      </MemoryRouter>,
+      root
+    );
+  
+    // Interact with the app
+    act(() => {
+      const homeLink = document.querySelector('#home-button');
+      homeLink.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+  
+    // Check result
+    expect(document.body.textContent).toBe("/home");
+});*/
