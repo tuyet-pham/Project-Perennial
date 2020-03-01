33f9f8d416018a265a85144d275287ea7500df35
Peyton Pritchard <pritchardpeyton119@gmail.com>
2020-03-01 10:05:38 -0600

    Routing tests in BottomNav.

--- reactfrontend/src/BottomNav.test.js
+++ reactfrontend/src/BottomNav.test.js
@@ -2,6 +2,7 @@ import ReactDOM, { unmountComponentAtNode } from 'react-dom';
 import React from 'react';
 import { render } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
+import { MemoryRouter } from 'react-router-dom';
 import BottomNav from './BottomNav';
 
 // Setup and teardown
@@ -17,12 +18,80 @@ afterEach(() => {
   container = null;
 });
 
-it('bottom nav renders', () => {
-  let container;
+test('navigates home when Home is selected', async => {
+  const root = document.createElement('div');
+  document.body.appendChild(root);
+
+  render(
+    <MemoryRouter>
+      <BottomNav />
+    </MemoryRouter>,
+    root
+  );
+
   act(() => {
-    container = render(<BottomNav />);
+    const link = document.querySelector('#home');
+    link.dispatchEvent(new MouseEvent('click', {bubbles: true}));
   });
-  expect(container.textContent).toBe('FOOTER');
+
+  expect(document.textContent).toBe('Home');
+});
+
+test('navigates to add-a-plant when Add a Plant is selected', async => {
+  const root = document.createElement('div');
+  document.body.appendChild(root);
+
+  render(
+    <MemoryRouter>
+      <BottomNav />
+    </MemoryRouter>,
+    root
+  );
+
+  act(() => {
+    const link = document.querySelector('#add-plant');
+    link.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+  });
+
+  expect(document.textContent).toBe('Add a Plant');
+});
+
+test('navigates to Monitor when Monitor is selected', async => {
+  const root = document.createElement('div');
+  document.body.appendChild(root);
+
+  render(
+    <MemoryRouter>
+      <BottomNav />
+    </MemoryRouter>,
+    root
+  );
+
+  act(() => {
+    const link = document.querySelector('#monitor');
+    link.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+  });
+
+  expect(document.textContent).toBe('Monitor');
+});
+
+test('navigates to Options when Options is selected', async => {
+  const root = document.createElement('div');
+  document.body.appendChild(root);
+
+  render(
+    <MemoryRouter>
+      <BottomNav />
+    </MemoryRouter>,
+    root
+  );
+
+  act(() => {
+    const link = document.querySelector('#options');
+    link.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+  });
+
+  expect(document.textContent).toBe('options');
 });
 
 /*
