0aeddfd95dbd7cbe5ac9a7dc719ffd2f9a499d85
Peyton Pritchard <pritchardpeyton119@gmail.com>
2020-03-19 14:49:28 -0500

    Add a Plant appearance

--- reactfrontend/src/pages/AddPlant.js
+++ reactfrontend/src/pages/AddPlant.js
@@ -5,16 +5,21 @@ import { FaCamera} from 'react-icons/fa'
 function AddPlant() {
     return (
       <PageTemplate>
+        <form>
           <div className="container-fluid">
             <div className="col form-container">
               <div id="basic-info" className="row">
                 <div className="col-3">
-                  <div className="empty-picture-icon">
+                  <div className="empty-picture-icon" onClick={addPlantIcon}>
                     <FaCamera />
                   </div>
                 </div>
                 <div className="col-9">
-                  Name
+                  <input 
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="Name..."
+                  />
                 </div>
               </div>
 
@@ -24,13 +29,21 @@ function AddPlant() {
                 Species
               </h2>
               <div id="species-info" className="row">
-                <div className="col-8">
-                  Dropdown
+                <div className="col">
+                  <select name="species" onChange={handleChange}>
+                    <option value="" disabled selected>Select species...</option>
+                    <option value="Aloe vera">Aloe Vera</option>
+                    <option value="Iris">Iris</option>
+                    <option value="Grass">Grass</option>
+                    <option value="Orchid">Orchid</option>
+                  </select>
                 </div>
               </div>
               <div className="row">
                 <div className="col">
-                  View suggested watering conditions...
+                  <a href="/add-plant">
+                    View suggested watering conditions...
+                  </a>
                 </div>
               </div>
 
@@ -41,18 +54,80 @@ function AddPlant() {
               </h2>
               <div id="geographic-data" className="row">
                 <div className="col">
-                  City text input
+                  <input 
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="City name..."
+                  />
                 </div>
                 <div className="col">
-                  State dropdown
+                <select name="species" onChange={handleChange}>
+                    <option value="" disabled selected>State</option>
+                    <option value="AL">AL</option>
+                    <option value="AK">AK</option>
+                    <option value="AZ">AZ</option>
+                    <option value="CA">CA</option>
+                    <option value="CO">CO</option>
+                    <option value="CT">CT</option>
+                    <option value="DE">DE</option>
+                    <option value="FL">FL</option>
+                    <option value="GA">GA</option>
+                    <option value="HI">HI</option>
+                    <option value="ID">ID</option>
+                    <option value="IL">IL</option>
+                    <option value="IN">IN</option>
+                    <option value="IA">IA</option>
+                    <option value="KS">KS</option>
+                    <option value="KY">KY</option>
+                    <option value="LA">LA</option>
+                    <option value="ME">ME</option>
+                    <option value="MD">MD</option>
+                    <option value="MA">MA</option>
+                    <option value="MI">MI</option>
+                    <option value="MN">MN</option>
+                    <option value="MS">MS</option>
+                    <option value="MO">MO</option>
+                    <option value="MT">MT</option>
+                    <option value="NE">NE</option>
+                    <option value="NV">NV</option>
+                    <option value="NH">NH</option>
+                    <option value="NJ">NJ</option>
+                    <option value="NM">NM</option>
+                    <option value="NY">NY</option>
+                    <option value="NC">NC</option>
+                    <option value="ND">ND</option>
+                    <option value="OH">OH</option>
+                    <option value="OK">OK</option>
+                    <option value="OR">OR</option>
+                    <option value="PA">PA</option>
+                    <option value="RI">RI</option>
+                    <option value="SC">SC</option>
+                    <option value="SD">SD</option>
+                    <option value="TN">TN</option>
+                    <option value="TX">TX</option>
+                    <option value="UT">UT</option>
+                    <option value="VT">VT</option>
+                    <option value="VA">VA</option>
+                    <option value="WA">WA</option>
+                    <option value="WV">WV</option>
+                    <option value="WI">WI</option>
+                    <option value="WY">WY</option>
+                  </select>
                 </div>
               </div>
               <div id="outdoor-mode" className="row">
                 <div className="col">
-                  Indoors radio
+                  <p>The plant is:</p>
                 </div>
                 <div className="col">
-                  Outdoors radio
+                  <label>
+                    <input type="radio" name="indoorsoutdors" value="indoors" onChange={handleChange} />Indoors
+                  </label>
+                </div>
+                <div className="col">
+                  <label>
+                    <input type="radio" name="indoorsoutdors" value="outdoors" onChange={handleChange} />Outdoors
+                  </label>
                 </div>
               </div>
 
@@ -61,17 +136,26 @@ function AddPlant() {
               <h2>
                 Watering conditions
               </h2>
-              <div id="watering-conditions" className="container">
-                <div className="row">
-                  Moisture level radio
+              <div id="watering-conditions" className="row">
+                <div className="col">
+                  <p>Water based on:</p>
                 </div>
-                <div className="row">
-                  Time elapsed level radio
+                <div className="col">
+                  <div className="row">
+                    <label>
+                      <input type="radio" name="condition" value="True" onChange={handleChange} />Moisture level
+                    </label>
+                  </div>
+                  <div className="row">
+                    <label>
+                      <input type="radio" name="condition" value="True" onChange={handleChange} />Time elapsed
+                    </label>
+                  </div>
                 </div>
-                <div className="row">
-                  Water after X time
+              </div>
+              <div>
+                  <SetWateringConditions/>
                   {/* Conditional render */}
-                </div>
               </div>
 
               <br />
@@ -81,13 +165,45 @@ function AddPlant() {
               </h2>
               <div id="additional-notes" className="row">
                 <div className="col">
-                  Add a Note
+                  <label>
+                    <textarea onChange={handleChange} placeholder="Add a note..." />
+                  </label>
                 </div>
               </div>
             </div>
           </div>
+        </form>
       </PageTemplate>
     );
 }
+
+function addPlantIcon() {
+  alert("Add plant icon");
+}
+
+function handleChange() {
+  console.log("Changed")
+}
   
-  export default AddPlant;
+function SetWateringConditions(props) {
+  const wateringCondition="time"
+  // const wateringCondition="moisture"
+
+  if(wateringCondition == "time") {
+    return (
+      <b>
+        Water after X days.
+      </b>
+    )
+  }
+  else {
+    return (
+      <b>
+        Water at X % moisture.
+      </b>
+    )
+  }
+
+}
+
+export default AddPlant;
