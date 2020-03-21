d019be5441c0d65ae53de8b54860202e38d0ba1e
tuyet-pham <tiipham19@gmail.com>
2020-03-21 06:35:51 -0500

    Changed settings and gitignore

--- djangobackend/account/models.py
+++ djangobackend/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
-# Create your models here.
+# # # Create your models here.
+
+# # class UserProfile(models.Model):  
+# #     user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
+# #     email = models.CharField(max_length=140)  
+# #     phone_number = models.CharField(max_length=140)  
+
+# #     def __unicode__(self):
+# #         return u'Profile of user: %s' % self.user.username
