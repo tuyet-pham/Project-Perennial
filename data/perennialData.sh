9dd7994e43bf1fc90d7a5bfcdb6f26acb0c93d89
Pedro Sanchez <chablo2499@gmail.com>
2020-03-30 13:41:11 -0500

    Fixed _users error where admin couldn't push users

--- data/perennialData.sh
+++ data/perennialData.sh
@@ -7,13 +7,18 @@ done
 HOST="http://admin:password1@db_data:5984"
 
 curl -X PUT $HOST/_users
-curl -X PUT $HOST/_users/01 -d '{"id":"1425", "info":{"username":"Johnny67","email":"johnny76@gmail.com","hashpass":"2%ff840mgjke"}}'
 curl -X PUT $HOST/_replicator
-curl -X PUT $HOST/_config
+curl -X PUT $HOST/users
+curl -X PUT $HOST/users/01 -d '{"id":"1425", "info":{"username":"Johnny67","email":"johnny76@gmail.com","hashpass":"2%ff840mgjke"}}'
+curl -X PUT $HOST/users/02 -d '{"id":"4233", "info":{"username":"Sarah23","email":"sarah23@gmail.com","hashpass":"6%sk384ldigd"}}'
 curl -X PUT $HOST/plant_device
-curl -X PUT $HOST/plant_device/01 -d '{"id":"4751","user_id":"142","type":"plant","location":{"country":"United States","state":"Texas","city":"Denton","IP":"129.120.67.52"},"species":"cactus","water_conditions":{"min_moisture_level":"20.45"}}'
+curl -X PUT $HOST/plant_device/01 -d '{"id":"4751","user_id":"1425","type":"plant","location":{"country":"United States","state":"Texas","city":"Denton","IP":"129.120.67.52"},"species":"Cactus","water_conditions":{"min_moisture_level":"0.60"}}'
+curl -X PUT $HOST/plant_device/02 -d '{"id":"1482","user_id":"1425","type":"plant","location":{"country":"United States","state":"Texas","city":"Denton","IP":"129.120.67.52"},"species":"Potato","water_conditions":{"min_moisture_level":"0.30"}}'
+curl -X PUT $HOST/plant_device/03 -d '{"id":"5439","user_id":"4233","type":"plant","location":{"country":"United States","state":"Oklahoma","city":"Tulsa","IP":"129.120.56.37"},"species":"Flower","water_conditions":{"min_moisture_level":"0.80"}}'
 curl -X PUT $HOST/plant_device_reading
-curl -X PUT $HOST/plant_device_reading/01 -d '{"id":"4751","user_id":"142","type":"plant","device_id":"1943","time_reading":"datetime","values":{"moisture_level":"70.35","water_level":"4.56","pump_status":"true"}}'
+curl -X PUT $HOST/plant_device_reading/01 -d '{"id":"4751","user_id":"1425","type":"plant","device_id":"1943","time_reading":"datetime","values":{"moisture_level":"0.90","water_level":"0.30","pump_status":"true"}}'
+curl -X PUT $HOST/plant_device_reading/02 -d '{"id":"1482","user_id":"1425","type":"plant","device_id":"5243","time_reading":"datetime","values":{"moisture_level":"0.60","water_level":"1.00","pump_status":"false"}}'
+curl -X PUT $HOST/plant_device_reading/03 -d '{"id":"5439","user_id":"4233","type":"plant","device_id":"9182","time_reading":"datetime","values":{"moisture_level":"1.00","water_level":"0.50","pump_status":"true"}}'
 curl -X PUT $HOST/plant_types
 curl -X PUT $HOST/plant_types/01 -d '{"plant":"African violet","min_moisture":"1.00"}'
 curl -X PUT $HOST/plant_types/02 -d '{"plant":"Baby’s tears","min_moisture":"1.00"}'
