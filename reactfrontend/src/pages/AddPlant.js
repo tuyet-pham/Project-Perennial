ee4e665e9e4ab62641f844fb690cd634cee338e8
Peyton Pritchard <pritchardpeyton119@gmail.com>
2020-04-20 15:11:17 -0500

    Un-overwrote AddPlant, sorry about that

--- reactfrontend/src/pages/AddPlant.js
+++ reactfrontend/src/pages/AddPlant.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import PageTemplate from '../PageTemplate';
 import { FaCamera} from 'react-icons/fa';
+import { addPlant } from '../api/AccountAPI'
+
 
 function AddPlant() {
     // Hooks for form values: name, species, geolocation, indoorsoutdoors, wateringConditionTrigger, wateringConditionValue, additionalNotes
@@ -17,14 +19,18 @@ function AddPlant() {
     const handleSubmit = (event) => {
       event.preventDefault()
       
-      console.log(name);
-      console.log(species);
-      console.log(geolocationCity);
-      console.log(geolocationState);
-      console.log(indoorsOutdoors);
-      console.log(wateringConditionTrigger);
-      console.log(wateringConditionValue);
-      console.log(additionalNotes);
+      const params = { 
+        name : `${name}`,
+        species : `${species}`,
+        geolocationCity :`${geolocationCity}`,
+        geolocationState :`${geolocationState}`,
+        indoorsOutdoors :`${indoorsOutdoors}`,
+        wateringConditionTrigger :`${wateringConditionTrigger}`,
+        wateringConditionValue :`${wateringConditionValue}`,
+        additionalNotes :`${additionalNotes}`
+      }
+      
+      addPlant(params);
     }
 
     // Forms with hooks reference: https://rangle.io/blog/simplifying-controlled-inputs-with-hooks/
