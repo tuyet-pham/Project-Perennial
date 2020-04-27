905117aaebebb9d4b68fbc250f680eeefe177f0a
tuyet-pham <tiipham19@gmail.com>
2020-04-27 02:54:42 -0500

    added avery's code back

--- djangobackend/notifications/views.py
+++ djangobackend/notifications/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+import requests
 from django.http import HttpResponse, JsonResponse
 from django.db import models
 
@@ -34,11 +35,14 @@ def sms(request):
 
 
 
-@csrf_exempt
-def email(request):
-    data = {}
-    try:
-        data = request.POST.get('device_id')
-    except Exception as e:
-        print(e)
-    return HttpResponse(data, status=HTTP_200_OK)
+# @csrf_exempt
+# def email(request):
+# 	return requests.post(
+# 		"https://api.mailgun.net/v3/sandbox2abd532106654910aca06b63e6b4d46d.mailgun.org/messages",
+# 		auth=("api", "c053bfc0c16dc4f497192184e35cff4b-f135b0f1-542b57b9"),
+# 		data={"from": "Excited User <Avery@https://api.mailgun.net/v3/sandbox2abd532106654910aca06b63e6b4d46d.mailgun.org>",
+# 			"to": "averyclariday44@gmail.com",
+# 			"subject": "Mailgun Test",
+# 			"text": "Testing Mailgun"
+# 			}
+#     )
--- djangobackend/tests/test_emails.py
+++ djangobackend/tests/test_emails.py
@@ -3,11 +3,10 @@ from django.test import TestCase
 from urls import urlpatterns
 from django.urls import reverse, resolve, path
 import requests
-from email_stuff.email_notification import send_simple_message
+# from notifications.views import email
 
-class test_emails(SimpleTestCase):
-
-    def test_simple_test(self):
-        response = send_simple_message()
-        self.assertEquals(response.status_code, 200)
+# class test_emails(SimpleTestCase):
+#     def test_simple_test(self):
+#         response = email()
+#         self.assertEquals(response.status_code, 200)
 
