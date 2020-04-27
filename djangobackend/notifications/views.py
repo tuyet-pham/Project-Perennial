111793a3cbca0625d9204049fc2943f84dd74f72
tuyet-pham <tiipham19@gmail.com>
2020-04-27 02:59:55 -0500

    uncommented email

--- djangobackend/notifications/views.py
+++ djangobackend/notifications/views.py
@@ -35,14 +35,14 @@ def sms(request):
 
 
 
-# @csrf_exempt
-# def email(request):
-# 	return requests.post(
-# 		"https://api.mailgun.net/v3/sandbox2abd532106654910aca06b63e6b4d46d.mailgun.org/messages",
-# 		auth=("api", "c053bfc0c16dc4f497192184e35cff4b-f135b0f1-542b57b9"),
-# 		data={"from": "Excited User <Avery@https://api.mailgun.net/v3/sandbox2abd532106654910aca06b63e6b4d46d.mailgun.org>",
-# 			"to": "averyclariday44@gmail.com",
-# 			"subject": "Mailgun Test",
-# 			"text": "Testing Mailgun"
-# 			}
-#     )
+@csrf_exempt
+def email(request):
+	return requests.post(
+		"https://api.mailgun.net/v3/sandbox2abd532106654910aca06b63e6b4d46d.mailgun.org/messages",
+		auth=("api", "c053bfc0c16dc4f497192184e35cff4b-f135b0f1-542b57b9"),
+		data={"from": "Excited User <Avery@https://api.mailgun.net/v3/sandbox2abd532106654910aca06b63e6b4d46d.mailgun.org>",
+			"to": "averyclariday44@gmail.com",
+			"subject": "Mailgun Test",
+			"text": "Testing Mailgun"
+			}
+    )
