54d284322f30dc94dc0fdfc023480bedbb4dd972
maggiojoe2 <thesealion95@gmail.com>
2020-05-02 09:44:02 -0500

    Revert "Added reload after login"
    This reverts commit 4c2e20564e3892aa8c55746297ed97442d0cbd85.

--- reactfrontend/src/components/loginform.js
+++ reactfrontend/src/components/loginform.js
@@ -14,35 +14,28 @@ function LoginForm(props) {
   const alert = useAlert()
 
   const handleSubmit = (evt) => {
-    console.log('Props: ' + props)
-    async function login(evt) {
-      evt.preventDefault();
+    evt.preventDefault();
 
-      if(challenge === true){
+    if(challenge === true){
 
-        console.log('Submitting Form...');
+      console.log('Submitting Form...');
 
-        const params = {
-          username : `${username}`,
-          password : `${password}`,
-        }
-
-        const logged_in = await userLogin(params, alert)
-
-
-        // setTimeout(() => {
-        //   if (localStorage.getItem('token') !== null){
-        //     history.push("/home");
-        //   }
-        // }, 1000);
-        window.location.reload(true)
+      const params = {
+        username : `${username}`,
+        password : `${password}`,
       }
-      else{
-        alert.error("You forgot about the Recaptcha!");
-      }
+
+      userLogin(params, alert);
+
+      // setTimeout(() => {
+      //   if (localStorage.getItem('token') !== null){
+      //     history.push("/home");
+      //   }
+      // }, 1000);
     }
-
-    login(evt)
+    else{
+      alert.error("You forgot about the Recaptcha!");
+    }
   }
 
   const capChange = (val) => {
