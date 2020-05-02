081cb97a2eb76c50bafa1126b7632b1f451d5744
maggiojoe2 <thesealion95@gmail.com>
2020-05-02 14:13:29 -0500

    Added test device to init for database.

--- data/perennialData.sh
+++ data/perennialData.sh
@@ -11,6 +11,7 @@ curl -X PUT $HOST/plant_device
 curl -X PUT $HOST/plant_device/01 -d '{"name":"pedro","username":"Sarah23","type":"plant","location":{"country":"United States","state":"Texas","city":"Denton","IP":"129.120.67.52"},"species":"Cactus","water_conditions":{"min_moisture_level":"0.60"}}'
 curl -X PUT $HOST/plant_device/02 -d '{"name":"pedro","username":"Sarah23","type":"plant","location":{"country":"United States","state":"Texas","city":"Denton","IP":"129.120.67.52"},"species":"Potato","water_conditions":{"min_moisture_level":"0.30"}}'
 curl -X PUT $HOST/plant_device/03 -d '{"name":"pedro","username":"Sarah23","type":"plant","location":{"country":"United States","state":"Oklahoma","city":"Tulsa","IP":"129.120.56.37"},"species":"Flower","water_conditions":{"min_moisture_level":"0.80"}}'
+curl -X PUT $HOST/plant_device/0d2186ea38e106dd06b16f95ee011150 -d '{"name": "Living Room", "username": "thesealion95", "species": "Begonia", "location": { "geolocationCity": "Irving", "geolocationState": "CO", "indoorsOutdoors": "indoors"}, "wateringConditionTrigger": "moisture", "wateringConditionValue": "50", "additionalNotes": ""}'
 curl -X PUT $HOST/plant_device_reading
 curl -X PUT $HOST/plant_device_reading/01 -d '{"type":"plant","device_id":"1943","time_reading":"datetime","values":{"moisture_level":"0.90","water_level":"0.30","pump_status":"true"}}'
 curl -X PUT $HOST/plant_device_reading/02 -d '{"type":"plant","device_id":"5243","time_reading":"datetime","values":{"moisture_level":"0.60","water_level":"1.00","pump_status":"false"}}'
