4c2e20564e3892aa8c55746297ed97442d0cbd85
maggiojoe2 <thesealion95@gmail.com>
2020-05-02 09:39:56 -0500

    Added reload after login

--- reactfrontend/src/components/loginform.js
+++ reactfrontend/src/components/loginform.js
@@ -14,28 +14,35 @@ function LoginForm(props) {
   const alert = useAlert()
 
   const handleSubmit = (evt) => {
-    evt.preventDefault();
+    console.log('Props: ' + props)
+    async function login(evt) {
+      evt.preventDefault();
 
-    if(challenge === true){
+      if(challenge === true){
 
-      console.log('Submitting Form...');
+        console.log('Submitting Form...');
 
-      const params = {
-        username : `${username}`,
-        password : `${password}`,
+        const params = {
+          username : `${username}`,
+          password : `${password}`,
+        }
+
+        const logged_in = await userLogin(params, alert)
+
+
+        // setTimeout(() => {
+        //   if (localStorage.getItem('token') !== null){
+        //     history.push("/home");
+        //   }
+        // }, 1000);
+        window.location.reload(true)
       }
-
-      userLogin(params, alert);
-
-      // setTimeout(() => {
-      //   if (localStorage.getItem('token') !== null){
-      //     history.push("/home");
-      //   }
-      // }, 1000);
+      else{
+        alert.error("You forgot about the Recaptcha!");
+      }
     }
-    else{
-      alert.error("You forgot about the Recaptcha!");
-    }
+
+    login(evt)
   }
 
   const capChange = (val) => {
